c35f52bb140b677022bb077c8d3d8f363d9fad29
Erik <erik.giesenloo@gmail.com>
2021-06-11 23:05:03 +0100

    Added switch between players

--- Main.gd
+++ Main.gd
@@ -5,23 +5,28 @@ onready var camera1 : Camera2D = $Player/Camera
 onready var camera2 : Camera2D = $Player2/Camera
 onready var camera3 : Camera2D
 
-# Called when the node enters the scene tree for the first time.
+onready var player1 : Player = $Player
+onready var player2 : Player = $Player2
+onready var player3 : Player
+
 func _ready():
   camera1.current = true
-  pass # Replace with function body.
+  player1.current = true
+  camera2.current = false
+  player2.current = false
 
 func _input(event : InputEvent) -> void:
   if event.is_action_pressed("player1"):
-    print("oi")
     camera1.current = true
     camera2.current = false
+
+    player1.current = true
+    player2.current = false    
   elif event.is_action_pressed("player2"):
-    print("oi")
     camera1.current = false
     camera2.current = true
+
+    player1.current = false
+    player2.current = true
   elif event.is_action_pressed("player3"):    
     pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
