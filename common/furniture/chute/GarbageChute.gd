55af3cf2a72173975eb9b1be6a6feb8502ec09b7
Erik <erik.giesenloo@gmail.com>
2021-06-12 21:03:04 +0100

    Added garbage chute phase 1

---
+++ common/furniture/chute/GarbageChute.gd
@@ -0,0 +1,16 @@
+extends Furniture
+class_name GarbageChute
+
+# Member variables
+export (String) var opened : String = "Chute is open"
+var chute_opened : bool = false
+
+func interact(body : Player) -> void:
+  if chute_opened and !item_collected and tool_name != "":
+    Util.swap_message(collected)
+    body.collect_tool(tool_name)
+    item_collected = true
+  elif body.tools.has("Butter knife"):
+    Util.swap_message(opened)
+    body.collect_tool(tool_name)
+    chute_opened = true
