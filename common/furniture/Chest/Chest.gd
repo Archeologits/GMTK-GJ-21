9efdce23e435bc01b3e874ce92298a0e6bc14659
Erik <erik.giesenloo@gmail.com>
2021-06-13 10:06:04 +0100

    Added chest

---
+++ common/furniture/Chest/Chest.gd
@@ -0,0 +1,9 @@
+extends Furniture
+
+func interact(body : Player) -> void:
+  if body.tools.has("Quill") and !item_collected:
+    Util.swap_message(collected)
+    body.collect_tool("Blue key")
+    item_collected = true
+    $Sprite.play("open")
+#    $Audio.play()
