2e991d54decc7a97f627f2e8aad9b173492e1353
Erik <erik.giesenloo@gmail.com>
2021-06-13 12:08:40 +0100

    Updated recipe book

--- common/book/RecipeBook.gd
+++ common/book/RecipeBook.gd
@@ -6,9 +6,18 @@ export (String) var message : String = "Press 'E' to interact"
 onready var destination : Position2D = get_node("../RecipeBookDestination")
 var last_player : Player
 
+var pushed_down : bool = false
+var item_collected : bool = false
+
 func interact(body : Player) -> void:
   # Push book down hole (move book to Blue Room Pantry)
-  position = destination.position
+  if !pushed_down:
+    position = destination.position
+  elif !item_collected:
+    body.collect_tool("Recipe")
+    Util.swap_message("Found recipe: 'Bread, Meat, Wine'")
+  else:
+    Util.swap_message("Recipe reads: 'Bread, Meat, Wine'")
   
 func _on_player_entered(body : Node2D) -> void:
   if body.is_in_group("Players"):
