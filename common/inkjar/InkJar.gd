13a40e3f45c9ee83606b103a8c8adb666fa13c86
Erik <erik.giesenloo@gmail.com>
2021-06-13 13:08:21 +0100

    Ink jar phase 1

--- common/inkjar/InkJar.gd
+++ common/inkjar/InkJar.gd
@@ -1,31 +1,11 @@
-extends Area2D
+extends Furniture
 class_name InkJar
 
-# Member variables
-export (String) var message : String = "Press 'E' to interact"
-onready var destination : Position2D = get_node("../RecipeBookDestination")
-var last_player : Player
-
-var pushed_down : bool = false
-var item_collected : bool = false
-
 func interact(body : Player) -> void:
-  # Push book down hole (move book to Blue Room Pantry)
-  if !pushed_down:
-    position = destination.position
-  elif !item_collected:
-    body.collect_tool("Recipe")
-    Util.swap_message("Found recipe: 'Bread, Meat, Wine'")
+  if body.tools.has("Quill") and !item_collected:
+    Util.swap_message(collected)
+    item_collected = true
+    # If audio added - simply uncomment the following
+#    $Audio.play()
   else:
-    Util.swap_message("Recipe reads: 'Bread, Meat, Wine'")
-  
-func _on_player_entered(body : Node2D) -> void:
-  if body.is_in_group("Players"):
-    Util.push_message(message)
-    body.interactible = self
-    last_player = body
-
-func _on_player_exited(body : Node2D) -> void:
-  if body == last_player:
-    Util.pop_message()
-    body.interactible = null
+    Util.shake()
