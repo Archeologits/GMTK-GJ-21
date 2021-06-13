e6a25ebc53cc40769e8d9ac401fe3333a361b1c8
Erik <erik.giesenloo@gmail.com>
2021-06-13 05:22:25 +0100

    Updated movable chair

--- common/furniture/Seat/Seat.gd
+++ common/furniture/Seat/Seat.gd
@@ -2,6 +2,7 @@ extends Furniture
 
 # Member variables
 var connected_to_player : bool = false
+var chair_under_crow : bool = false
 var distance_to_player : Vector2
 
 func _physics_process(delta : float) -> void:
@@ -12,22 +13,17 @@ func interact(body : Player) -> void:
   # Item moves with player
   connected_to_player = !connected_to_player # toggle connection
   if connected_to_player:
+    $Collision.disabled = true # Disable collision -> player can "push" chair
     last_player = body
     distance_to_player = position - body.position
-    
-#  set_physics_process(true)
-#  print("yes")
 
+func _on_crow_entered(area : Area2D) -> void:
+  if area.get_parent().name == "Crow":
+    print("crow entered")
+    chair_under_crow = true
+#    $Collision.disabled = true # Disable collision -> player can "climb" chair
 
-#func _on_player_entered(body : Node2D) -> void:
-#  if !item_collected and body.is_in_group("Players"):
-#    Util.push_message(message)
-#    body.interactible = self
-#    last_player = body
-#
-#func _on_player_exited(body : Node2D) -> void:
-#  if interactible and body == last_player:
-#    Util.pop_message()
-#    body.interactible = null
-#    if item_collected:
-#      interactible = false
+func _on_crow_exited(area : Area2D) -> void:
+  if area.get_parent().name == "Crow":
+    chair_under_crow = false
+    print("crow exited")
